a8e72d19c36e268661062e89ee52aa24b7b71d56
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-26 19:50:57 -0500

    checking to make sure .env is not there

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 
-const routes = require('./routes');
-const path = require ('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+sequelize.sync({force:true}).then(()=> {
+  app.listen(PORT, ()=> {
+    console.log(`helloooo!!! it's workin ${PORT}`);
+  });
+});
