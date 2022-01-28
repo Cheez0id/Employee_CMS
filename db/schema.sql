c28cc14b225c96a17ef247b5da03c5cd27697a60
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-27 20:29:53 -0500

    created database with 3 tables and seeded them

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,29 @@
   USE company_db;
 
 CREATE TABLE departments (
-    dept_id INT NOT NULL,
+    id INT NOT NULL,
     dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (dept_id)
+    PRIMARY KEY (id)
 );
 
 
 CREATE TABLE roles (
-    role_id INT NOT NULL,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     department INT,
-    salary INT NOT NULL,
+    salary DECIMAL(4,2),
     FOREIGN KEY (department)
-    REFERENCES departments(dept_id),
-    PRIMARY KEY (role_id)
+    REFERENCES departments(id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
-    emp_id INT NOT NULL,
+    id INT NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles (role_id),
-    PRIMARY KEY (emp_id)
+    REFERENCES roles (id),
+    PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO departments (id, dept_name)
+VALUES (01, "customer service"),
+       (02, "production"),
+       (03, "sales"),
+       (04, "quality");
+
+INSERT INTO roles (id, title, department, salary)
+VALUES (010, "customer rep", 01, 14.00),
+       (020, "processor", 02, 08.00),
+       (030, "sales rep", 03, 1000.00 ),
+       (040, "qa engineer", 04, 09.00);
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (001, "Brad", "Majors", 010, 52),
+       (002, "Janet", "Weiss", 010, 52),
+       (003, "Frank", "Furter", 030, 98 ),
+       (004, "Rocky", "Horror", 040, 27);
