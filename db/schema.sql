1d23fc4e4ea7a7dfc1cf4e38d5e2e72b57a40d83
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-27 20:11:59 -0500

    girl I made that database with 3 tables yogit add -A using sql only.  should I go ahead to sequelize? hmm

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,32 @@
   DROP DATABASE IF EXISTS company_db;
   CREATE DATABASE company_db;
+
+  USE company_db;
+
+CREATE TABLE departments (
+    dept_id INT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (dept_id)
+);
+
+
+CREATE TABLE roles (
+    role_id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    department INT,
+    salary INT NOT NULL,
+    FOREIGN KEY (department)
+    REFERENCES departments(dept_id),
+    PRIMARY KEY (role_id)
+);
+
+CREATE TABLE employees (
+    emp_id INT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles (role_id),
+    PRIMARY KEY (emp_id)
+);
---
+++ index.js
@@ -0,0 +1,4 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
