3ce46eb6d82cc9c5ec722029da95809616768008
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-28 22:06:55 -0500

    I WAS ABLE TO PUT THE DATA FROM THE DATABASE THROUGH TO localhost:3001/api/companygit add -A

--- server.js
+++ server.js
@@ -14,7 +14,28 @@ var con = mysql.createConnection({
   database: process.env.DB_SRC,
 });
 
+con.connect((err) => {
+  if (err){
+    throw err;
+  }
+  console.log('mysql is connected?')
+})
 
+
+app.get('/api/company', (req, res) => {
+  const sql = `SELECT * from employees`;
+  
+con.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 //username/passwords hidden via sequelize using .env 
 sequelize.sync({force:false}).then(()=> {
