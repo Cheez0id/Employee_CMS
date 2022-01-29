724a4495750fef539d2284361848ea2a41463d08
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-01-28 20:47:14 -0500

    more setup for inquirer

--- index.js
+++ index.js
@@ -3,22 +3,38 @@ const fs = require("fs");
 const console_t = require("console.table");
 
 //from the CMS_Employess the mysql route for schema and seeds is source ./db/schema and source  
+const hello = () => {console.log("Mary is amazing!")};
 
 const firstChoice = [
   {
 		type: "list",
-		name: "view",
+		name: "first",
 		message: "Hello, what would you like to do?",
+    choices: ["View", "Add", "Update"],
+	}
+]
+const viewChoice = [
+  {
+		type: "list",
+		name: "view",
+		message: "What do you want to view?",
     choices: ["View All Roles", "View All Employees", "View all Departments"],
 	}
 ]
 const init = () =>{
   inquirer.prompt(firstChoice).then((choice) =>{
-      if(choice.view === undefined){
+      if(choice.first === undefined){
       console.log("lol, it's undefined!")
     }
-      else if (choice.view === "View All Roles"){
-        console.log("lol, that is not written yet!")
+      else if (choice.first === "View"){
+        inquirer.prompt(viewChoice).then (hello        
+  )
+      }
+      else if (choice.first === "Add"){
+        console.log("lol, add is not written yet!")
+      }
+      else if (choice.first === "Update"){
+        console.log("lol, update is not written yet!")
       }
   })
 }
